0cbcac80c168ade9ac4becfe43d2390237eb76c6
vsg-github <vsrinivasagopalan@hotmail.com>
2024-07-07 00:18:04 +0530

    Update streamlit_app.py
    pandas

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,6 +2,7 @@
 import streamlit as st
 from snowflake.snowpark.functions import col
 import requests
+import pandas
 
 # Write directly to the app
 st.title(":cup_with_straw: Customize your Smoothie! :cup_with_straw:")
